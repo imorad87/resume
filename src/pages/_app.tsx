d74687c48834d337e111fb096e4c29b7278f2d11
Islam Morad <imorad87@gmail.com>
2022-06-13 05:08:34 +0200

    config commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../theme';
+import { AppProps } from 'next/app';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+
+export default function MyApp({Component, pageProps}: AppProps) {
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
